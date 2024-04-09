bd3a1cba966175f3b25c10428f374285aa554e71
navin-net <netnavin70@gmail.com>
2024-04-09 18:12:52 +0700

    final-2

--- testing.php
+++ testing.php
@@ -3,7 +3,7 @@
 $event_id = $this->input->get('event_id') ? $this->input->get('event_id') : null;
 
 $ev = "( SELECT ev.title, ev.start_date, ev.end_date, s.name from 
- {$this->db->dbprefix('md_events')} ev
+{$this->db->dbprefix('md_events')} ev
         left JOIN " . $this->db->dbprefix('md_event_speakers') . ' es ON ev.id = es.event_id
         left join ' . $this->db->dbprefix('md_speakers') . ' s ON es.speaker_id = s.id ';
 if ($start_date) {
@@ -12,8 +12,9 @@ if ($start_date) {
                 $ev .= " ev.date >= '{$start_date}' AND ev.date < '{$end_date}' ";
                 }
                 $ev .= ' GROUP BY pp.event_id ) PCosts';
+
 if ($pdf || $xls) {
-    $this->db->select($this->db->dbprefix('md_event_registers'))
+    $this->db->select($this->db->dbprefix('md_event_registers').'.name,')
 }
 
 
